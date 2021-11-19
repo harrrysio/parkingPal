5f5d8bd92d5a64730c74bff6b578af6d5b5708bd
harrrysio <72060634+harrrysio@users.noreply.github.com>
2021-11-18 19:57:40 -0800

    asdad

--- project 3/sketch.js
+++ project 3/sketch.js
@@ -4,7 +4,9 @@ let latitude;
 let parkingData;
 let latlon;
 let status;
-
+var mapx = 0, mapy = 0, mx = 0, my = 0;
+var strName = "";
+var showText = false;
 const mappa = new Mappa('Leaflet');
 const options = {
   lat: 37.75,
@@ -12,41 +14,25 @@ const options = {
   zoom: 13,
   style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
 }
-
-
 function setup() {
   canvas = createCanvas(428, 926);
   myMap = mappa.tileMap(options);
   myMap.overlay(canvas)
-
   fill(200, 100, 100);
-
   // Only redraw the point when the map changes and not every frame.
   // myMap.onChange(drawPoint);
-
   loadJSON('https://data.sfgov.org/resource/8vzz-qzz9.json', gotData);
-
-
 }
-
 function gotData(data) {
   parkingData = data;
-
 }
-
-
-
-
 function draw() {
-
   clear();
-
   if (parkingData) {
     for (var i = 0; i < parkingData.length; i++) {
       let latlon = parkingData[i].shape.coordinates
       let status = parkingData[i].active_meter_flag
-      console.log(latlon);
-      console.log(status);
+      let streetName = parkingData[i].street_name
       const nigeria = myMap.latLngToPixel(latlon[1], latlon[0]);
       if (status === 'M'){
         fill(0,255,0);
@@ -54,15 +40,29 @@ function draw() {
       else if (status === 'T'){
         fill(255,0,0);
       }
-
       else if (status === 'U'){
         fill(0,0,255);
       }
-
-      ellipse(nigeria.x, nigeria.y, 5, 5);
-    }
-
+      ellipse(nigeria.x, nigeria.y, 15, 15);
+        if(showText){
+          textSize(35);
+    text(strName, mx, my);
   }
-
-
+          if(mouseX >= nigeria.x && mouseX <= nigeria.x + 15  && mouseY >= nigeria.y && mouseY <= nigeria.y + 15 && !showText){
+  // fill('black');
+            // textSize(35);
+            // print(streetName);
+            mapx = nigeria.x
+            mapy = nigeria.y
+            strName = streetName;
+}
+    }
+  }
+}
+function mousePressed() {
+            if(mouseX >= mapx && mouseX <= mapx + 15  && mouseY >= mapy && mouseY <= mapy + 15){
+              mx = mapx;
+              my = mapy;
+              showText = !showText
+            }
 }
